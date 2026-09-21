11e4a975546c33f88a8df7ca96b55cf2155210a7
Olga Kudrina <nityanka92@gmail.com>
2026-09-21 23:10:47 +0300

    homework4 is done

---
+++ homework/olga_kudrina/Homework_4/hw04.py
@@ -0,0 +1,10 @@
+my_dict = {'tuple': (1, 2, 3, 4, 5), 'list': ['one', 'two', 7, 95, False], 'dict': {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}, 'set': {3, 4, 5, 6, 7}}
+
+print(my_dict['tuple'][-1])
+my_dict['list'].append(42)
+my_dict['list'].pop(1)
+my_dict['dict']['six'] = '7'
+del my_dict['dict']['one']
+my_dict['set'].add(42)
+my_dict['set'].pop()
+print(my_dict)
