79b16a88d1fb77ee1ef9e62372ea352142e3625e
Olga Kudrina <nityanka92@gmail.com>
2026-09-21 23:20:33 +0300

    lint fix

--- homework/olga_kudrina/Homework_4/hw04.py
+++ homework/olga_kudrina/Homework_4/hw04.py
@@ -1,4 +1,7 @@
-my_dict = {'tuple': (1, 2, 3, 4, 5), 'list': ['one', 'two', 7, 95, False], 'dict': {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}, 'set': {3, 4, 5, 6, 7}}
+my_dict = {'tuple': (1, 2, 3, 4, 5),
+           'list': ['one', 'two', 7, 95, False],
+           'dict': {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5},
+           'set': {3, 4, 5, 6, 7}}
 
 print(my_dict['tuple'][-1])
 my_dict['list'].append(42)
